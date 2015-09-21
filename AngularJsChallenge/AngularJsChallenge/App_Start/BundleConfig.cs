b7f3a794e83b0e3b99868f1091291a4c9d86404a
Nhan Phan <nhanphan@futurify.vn>
2015-09-21 15:04:04 +0700

    add project

---
+++ AngularJsChallenge/AngularJsChallenge/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AngularJsChallenge
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/main").Include(
+                       "~/Scripts/jquery-2.1.1.js",
+                       "~/Scripts/bootstrap.min.js",                       
+                       "~/Scripts/angular.min.js"
+                       ));
+
+            bundles.Add(new StyleBundle("~/Styles/main").Include(
+                        "~/Styles/bootstrap.css"                      
+                        ));
+            BundleTable.EnableOptimizations = false;
+            bundles.IgnoreList.Clear();
+        }
+    }
+}
---
+++ AngularJsChallenge/AngularJsChallenge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularJsChallenge.MvcApplication" Language="C#" %>
